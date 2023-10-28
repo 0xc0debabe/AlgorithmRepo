84b4a9112332d1c9085e0183150df3174d73d102
0xc0de1and <kongminoo@naver.com>
2023-10-28 12:27:39 +0900

    231028_baekjoon

--- src/Silver/II/Bertrand.java
+++ src/Silver/II/Bertrand.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class bertrand {
+public class Bertrand {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
