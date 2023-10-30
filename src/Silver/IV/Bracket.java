b347468e1ab4ce351c7952c3d967b830afa8f9a2
0xc0de1and <kongminoo@naver.com>
2023-10-30 21:55:53 +0900

    231030_baekjoon

---
+++ src/Silver/IV/Bracket.java
@@ -0,0 +1,36 @@
+package Silver.IV;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Bracket {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            sb.append(poppush(br.readLine())).append("\n");
+
+        }
+        System.out.println(sb);
+    }
+
+    static String poppush(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return "NO";
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.size() == 0 ? "YES" : "NO";
+    }
+}
