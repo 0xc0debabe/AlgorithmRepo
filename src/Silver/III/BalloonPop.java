06c18928d4884b0fc5a67a79414cb4ed5919abaa
0xc0de1and <kongminoo@naver.com>
2023-11-07 23:21:21 +0900

    231107_baekjoon

--- src/Silver/III/BalloonPop.java
+++ src/Silver/III/BalloonPop.java
@@ -8,49 +8,57 @@ import java.util.*;
 public class BalloonPop {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<int[]> deque = new ArrayDeque<>();
         int n = Integer.parseInt(br.readLine());
-
-        Deque<int[]> q = new ArrayDeque<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
         int[] arr = new int[n];
-        for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(stk.nextToken());
         }
 
         StringBuilder sb = new StringBuilder();
-        sb.append("1 ");
-        int in = arr[0];
+//        sb.append("1 ");
+//        int change = arr[0];
 
-        for(int i=1; i<n; i++){
-            q.add(new int[] {(i+1), arr[i]}); // {풍선 idx, 내용}
+        for (int i = 0; i < n; i++) {
+            deque.add(new int[] {(i + 1 ), arr[i]});
         }
 
-        while(!q.isEmpty()) {
-            // 양수인 경우
-            if(in >0) {
-                // 순서 뒤로 돌리기
-                for(int i=1; i<in; i++) {
-                    q.add(q.poll());
+        boolean changed = false;
+        int change = 0;
+        while (!deque.isEmpty()) {
+            int peek = deque.peek()[0];
+            int peek1 = deque.peek()[1];
+            if (peek1 > 0) {
+                sb.append(peek).append(" ");
+                change = deque.poll()[1];
+            } else {
+                List<int[]> tempList = new LinkedList<>();
+                while (!deque.isEmpty()) {
+                    tempList.add(deque.pollLast());
                 }
 
-                int[] nxt = q.poll();
-                in = nxt[1];
-                sb.append(nxt[0]+" ");
+                for (int[] element : tempList) {
+                    deque.add(element);
+                }
+                int peek3 = deque.peekLast()[0];
+                sb.append(peek3).append(" ");
+                change = deque.pollLast()[1];
+                changed = true;
             }
-            // 음수인 경우
+            if (change > 0) {
+                for (int i = 0; i < change - 1; i++) {
+                    deque.add(deque.poll()); //pop : LIFO poll : FIFO
+                }
+            }
             else {
-                for(int i=1; i<-in; i++	) {
-                    q.addFirst(q.pollLast());
+                for (int i = 0; i < change * -1 - 1; i++) {
+                    deque.add(deque.poll());
+//                    deque.addFirst(deque.pollLast());
                 }
-
-                int[] nxt = q.pollLast();
-                in = nxt[1];
-                sb.append(nxt[0]+" ");
             }
         }
 
-        System.out.println(sb.toString());
-
+        System.out.println(sb);
     }
 }
