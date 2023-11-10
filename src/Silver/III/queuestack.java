2db18c039549e851727308b294d01abd6b9eff49
0xc0de1and <kongminoo@naver.com>
2023-11-11 02:07:38 +0900

    231111_baekjoon

--- src/Silver/III/queuestack.java
+++ src/Silver/III/queuestack.java
@@ -12,38 +12,27 @@ public class queuestack {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         Deque<Integer> deque = new ArrayDeque<>();
-        int[] queueorstackarr = new int[n];
-        int[] answer = new int[n];
+        int[] dequearr = new int[n];
         StringTokenizer stk = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            queueorstackarr[i] = Integer.parseInt(stk.nextToken());
+            dequearr[i] = Integer.parseInt(stk.nextToken());
         }
         stk = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            deque.add(Integer.parseInt(stk.nextToken()));
-        }
-
-        int m = Integer.parseInt(br.readLine());
-        stk = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < m; i++) {
-            int saveNum = Integer.parseInt(stk.nextToken());
-            for (int j = 0; j < queueorstackarr.length; j++) {
-                if (queueorstackarr[j] == 0) {
-                    deque.add(saveNum);
-                    saveNum = deque.pop();
-                    answer[i] = saveNum;
-                }
-                else {
-                    answer[i] = deque.peekFirst();
-                    deque.add(deque.pop());
-                }
+            int a = Integer.parseInt(stk.nextToken());
+            if (dequearr[i] == 0) {
+                deque.addLast(a);
             }
         }
 
-        for (int i = 0; i < m-1; i++) {
-            System.out.print(answer[i] + " ");
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int a = Integer.parseInt(stk.nextToken());
+            deque.addFirst(a);
+            sb.append(deque.pollLast()).append(" ");
         }
-        System.out.print(answer[m-1]);
+        System.out.println(sb);
     }
 }
