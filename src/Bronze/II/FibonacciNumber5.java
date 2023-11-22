d821e511807d98065a9b5822e040985e2a75a4f8
0xc0de1and <kongminoo@naver.com>
2023-11-22 19:16:30 +0900

    231122_baekjoon

--- src/Bronze/II/FibonacciNumber5.java
+++ src/Bronze/II/FibonacciNumber5.java
@@ -1,17 +1,19 @@
-package Bronze.V;
+package Bronze.II;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Factorial {
+public class FibonacciNumber5 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long answer = 1;
-        for (int i = 1; i <= n; i++) {
-            answer *= i;
-        }
-        System.out.println(answer);
+        System.out.println(fibonacci(n));
+    }
+
+    static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
