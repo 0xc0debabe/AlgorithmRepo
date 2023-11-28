1ead978c3cba183a84a07f6f588cf39bae26d7d4
0xc0de1and <kongminoo@naver.com>
2023-11-28 22:57:35 +0900

    231128_baekjoon

--- src/Gold/V/Stargazing10.java
+++ src/Gold/V/Stargazing10.java
@@ -5,33 +5,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Stargazing10 {
-    static StringBuilder sb = new StringBuilder();
-    static char[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        arr = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = '*';
+        int N = Integer.parseInt(br.readLine());
+
+        char[][] stars = new char[N][N];
+
+        // 배열 초기화
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                stars[i][j] = ' ';
             }
         }
-        star(arr, n / 3);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j]);
-            }
-            System.out.println();
+
+        // 재귀 함수 호출
+        drawStars(stars, 0, 0, N);
+
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(stars[i]);
+            sb.append('\n');
         }
+
+        System.out.println(sb.toString());
     }
-    static void star(char[][] chars, int n) {
-        if (n >= 1) {
-            for (int i = n; i < 2 * n; i++) {
-                for (int j = n; j < 2 * n; j++) {
-                    chars[i][j] = ' ';
+
+    static void drawStars(char[][] stars, int row, int col, int size) {
+        if (size == 1) {
+            stars[row][col] = '*';
+            return;
+        }
+
+        int newSize = size / 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue; // 가운데 칸은 비워둠
                 }
+                drawStars(stars, row + i * newSize, col + j * newSize, newSize);
             }
-            star(chars, n / 3);
         }
     }
 }
