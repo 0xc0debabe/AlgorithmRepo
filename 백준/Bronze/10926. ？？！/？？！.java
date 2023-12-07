37a8d4c506f7c9cacdc0b7b2dec5e8d5c291ed58
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2023-12-07 21:22:57 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+        sb.append(s).append("??!");
+        System.out.println(sb);
+
+
+    }
+
+}
