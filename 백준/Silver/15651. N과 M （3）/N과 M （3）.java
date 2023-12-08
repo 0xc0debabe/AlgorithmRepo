bac4ceaaedab56ebc8876a11ec120ca94f6af9b8
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2023-12-09 00:49:35 +0900

    [Silver III] Title: N과 M (3), Time: 464 ms, Memory: 68488 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+        arr = new int[m];
+        findNumber(0);
+        System.out.println(sb);
+    }
+
+    static void findNumber(int index) {
+        if (index == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            arr[index] = i;
+            findNumber(index + 1);
+        }
+    }
+}
