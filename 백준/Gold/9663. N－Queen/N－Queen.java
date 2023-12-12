e84058c86f07e1e8bb135207341469c717e07dd2
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2023-12-12 21:59:07 +0900

    [Gold IV] Title: N-Queen, Time: 5176 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    static int[] arr;
     static int n;
     static int cnt = 0;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
@@ -14,25 +15,26 @@ public class Main {
         System.out.println(cnt);
     }
 
-    static void nQueen(int index) {
-        if (index == n) {
+    static void nQueen(int col) {
+        if (col == n) {
             cnt++;
             return;
         }
 
         for (int i = 0; i < n; i++) {
-            arr[index] = i;
-            if (Possibility(index)) {
-                nQueen(index + 1);
+            arr[col] = i;
+            if (possible(col)) {
+                nQueen(col + 1);
             }
         }
+
     }
 
-    public static boolean Possibility(int col) {
+    static boolean possible(int col) {
         for (int i = 0; i < col; i++) {
-            if (arr[col] == arr[i]) {
+            if (arr[i] == arr[col]) {
                 return false;
-            } else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+            } else if (Math.abs(arr[i] - arr[col]) == Math.abs(col - i)) {
                 return false;
             }
         }
