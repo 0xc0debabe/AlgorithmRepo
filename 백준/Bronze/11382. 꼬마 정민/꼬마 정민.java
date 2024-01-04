fbc6f67a9df3c9df9645eee3f5904834e00e01e6
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-01-04 18:39:40 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Long a = scn.nextLong();
+        Long b = scn.nextLong();
+        Long c = scn.nextLong();
+        System.out.println(a+b+c);
+    }
+}
