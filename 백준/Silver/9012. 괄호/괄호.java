9659be0205bf624c28076bc0b8a5050ca0d87c69
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-03-25 18:12:07 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            sb.append(poppush(br.readLine())).append("\n");
+
+        }
+        System.out.println(sb);
+    }
+
+    static String poppush(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return "NO";
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.size() == 0 ? "YES" : "NO";
+    }
+}
