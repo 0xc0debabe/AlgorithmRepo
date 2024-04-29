9dfbfdff4535ad5ee3ebd23f7ad35ac34abb5d3a
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-04-29 13:35:00 +0900

    [Gold II] Title: 트리의 지름, Time: 936 ms, Memory: 99692 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Node {
+    int linkedNum;
+    int distance;
+
+    public Node(int linkedNum, int distance) {
+        this.linkedNum = linkedNum;
+        this.distance = distance;
+    }
+}
+
+public class Main {
+    static boolean[] visited;
+    static List<Node>[] lists;
+    static int[] distanceArr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        lists = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            lists[i] = new ArrayList<>();
+        }
+
+        StringTokenizer stk;
+        for (int i = 1; i <= n; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int vertex = Integer.parseInt(stk.nextToken());
+            while (true) {
+                int num = Integer.parseInt(stk.nextToken());
+                if (num == -1) {
+                    break;
+                }
+                int distance = Integer.parseInt(stk.nextToken());
+                lists[vertex].add(new Node(num, distance));
+            }
+        }
+        visited = new boolean[n + 1];
+        distanceArr = new int[n + 1];
+
+        bfs(1);
+        int max = Integer.MIN_VALUE;
+        int tmp = 0;
+        for (int i = 1; i <= n; i++) {
+            if (distanceArr[i] > max) {
+                max = distanceArr[i];
+                tmp = i;
+            }
+        }
+        Arrays.fill(distanceArr, 0);
+        Arrays.fill(visited, false);
+        bfs(tmp);
+        Arrays.sort(distanceArr);
+        System.out.println(distanceArr[n]);
+    }
+
+    static void bfs(int n) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(n);
+        visited[n] = true;
+
+        while (!deque.isEmpty()) {
+            int poll = deque.pollLast();
+
+            for (Node node : lists[poll]) {
+                int num = node.linkedNum;
+                int dist = node.distance;
+
+                if (!visited[num]) {
+                    visited[num] = true;
+                    deque.addFirst(num);
+                    distanceArr[num] = distanceArr[poll] + dist;
+                }
+            }
+        }
+    }
+}
