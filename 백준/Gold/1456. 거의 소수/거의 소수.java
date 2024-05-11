6f3616b18b1634fbfebf96a3ea275c397df38fac
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-05-11 19:36:49 +0900

    [Gold V] Title: 거의 소수, Time: 320 ms, Memory: 27524 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.java
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -8,31 +8,44 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
 
-        long n = Long.parseLong(stk.nextToken());
-        long m = Long.parseLong(stk.nextToken());
-        long[] isPrime = new long[10000001];
+        long start = Long.parseLong(stk.nextToken());
+        long end = Long.parseLong(stk.nextToken());
+
+        boolean[] isPrime = new boolean[10000001];
 
         for (int i = 2; i < isPrime.length; i++) {
-            isPrime[i] = i;
+            isPrime[i] = true;
         }
 
         for (int i = 2; i <= Math.sqrt(isPrime.length); i++) {
-            if (isPrime[i] == 0) continue;
+            if (!isPrime[i]) continue;
 
             for (int j = 2 * i; j < isPrime.length; j += i) {
-                isPrime[j] = 0;
+                isPrime[j] = false;
             }
         }
+
+
         int cnt = 0;
         for (int i = 2; i < 10000001; i++) {
-            if (isPrime[i] != 0) {
-                long temp = isPrime[i];
-                while ((double) isPrime[i] <= (double) m / (double) temp) {
-                    if ((double) isPrime[i] >= (double) n / (double) temp) {
+            if (!isPrime[i]) continue;
+            if (i > end) break;
+
+            if (isPrime[i]) {
+                int pow = 2;
+
+                while (true) {
+                    long tmp = (long) Math.pow(i, pow);
+                    if (tmp >= Math.pow(10, 17)) {
+                        break;
+                    }
+
+                    if (tmp <= end && tmp >= start) {
                         cnt++;
                     }
-                    temp = temp * isPrime[i];
+                    pow++;
                 }
+
             }
         }
         System.out.println(cnt);
