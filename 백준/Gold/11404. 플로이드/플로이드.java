246f28e82d2a049f0f1d2100ade143726f75de2b
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-05-23 17:09:38 +0900

    [Gold IV] Title: 플로이드, Time: 644 ms, Memory: 44892 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] distance = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    distance[i][j] = 0;
+                } else {
+                    distance[i][j] = 10_000_001;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            if (distance[s][e] > v) {
+                distance[s][e] = v;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    if (distance[j][k] > distance[j][i] + distance[i][k]) {
+                        distance[j][k] = distance[j][i] + distance[i][k];
+                    }
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (distance[i][j] == 10_000_001) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print(distance[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
