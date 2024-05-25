3dfe1e8c5f3a723a9c298c8f3ac4f9dbe5af87a9
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-05-25 16:08:51 +0900

    [Gold V] Title: 트리, Time: 120 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -2,34 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class Main {
     static List<List<Integer>> lists;
     static boolean[] isVisited;
-    static int answer = 0;
+    static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        isVisited = new boolean[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         lists = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
             lists.add(new ArrayList<>());
         }
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        isVisited = new boolean[n];
         int root = 0;
         for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            if (x == -1){
+            int num = Integer.parseInt(st.nextToken());
+            if (num == -1) {
                 root = i;
                 continue;
             }
-            lists.get(i).add(x);
-            lists.get(x).add(i);
+            lists.get(i).add(num);
+            lists.get(num).add(i);
         }
+
         int k = Integer.parseInt(br.readLine());
 
         if (k == root) {
@@ -38,21 +36,20 @@ public class Main {
             dfs(root, k);
             System.out.println(answer);
         }
-
-
     }
 
     static void dfs(int n, int k) {
         isVisited[n] = true;
-        int now = 0;
+        int cnt = 0;
 
         for (int value : lists.get(n)) {
             if (!isVisited[value] && value != k) {
-                now++;
+                cnt++;
                 dfs(value, k);
             }
         }
-        if (now == 0) {
+
+        if (cnt == 0) {
             answer++;
         }
     }
