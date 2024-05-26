1f9acfab16695e8fd63ce8b2f13bce21a15bbaf4
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-05-26 17:18:27 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,24 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stk = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(stk.nextToken());
-        int m = Integer.parseInt(stk.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        // 이항계수 (N
-//                 K) 란  nCk = n! / (n-k)! * k! n개중 k 선택(단, 중복X, 순서X)
-//        공 10개중 3개를 뽑는 경우의 수 10 9 8 / 3 2 1  ( 3 2 1 인 경우는 빨 파 노 일경우 빨을 선택했을때 파노가 남기때문에 중복제거)
-        int answer = 1;
         int idx = 1;
-        for (int i = n; i > m; i--) {
-            answer *= i;
-            answer /= idx++;
+        int answer = 1;
+        int tmp = 1;
+        for (int i = n; i > n - k; i--) {
+            answer *= i % 10_007;
+            tmp *= idx++ % 10_007;
         }
-        System.out.println(answer);
+
+        System.out.println(answer / tmp % 10_007);
     }
 }
