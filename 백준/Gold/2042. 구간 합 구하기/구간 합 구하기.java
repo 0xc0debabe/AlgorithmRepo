ba5e9c7491675eba9f2901c38c371f0d6d3bd1bb
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-05-26 22:08:26 +0900

    [Gold I] Title: 구간 합 구하기, Time: 712 ms, Memory: 98724 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -3,10 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+import static java.lang.Math.*;
 
 public class Main {
     static long[] tree;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,58 +14,57 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int treeHeight = 0;
-        int length = n;
-        while (length != 0) {
-            length /= 2;
-            treeHeight++;
-        }
-        int treeSize = (int) Math.pow(2, treeHeight + 1);
-        int leftNodeStartIndex = treeSize / 2 - 1;
+//        int treeHeight = 0;
+//        int length = n;
+//        while (length != 0) {
+//            length /= 2;
+//            treeHeight++;
+//        }
+//        int treeSize = (int) Math.pow(2, treeHeight + 1);\
+//        double v = log(n) / log(2);
+//        System.out.println(v);
+        int treeSize = (int) pow(2,  (ceil(log(n) / log(2))) + 1);
+        int leftStartIdx = treeSize / 2 - 1;
         tree = new long[treeSize + 1];
-        for (int i = leftNodeStartIndex + 1; i <= leftNodeStartIndex + n; i++) {
+        for (int i = leftStartIdx + 1; i <= leftStartIdx + n; i++) {
             tree[i] = Long.parseLong(br.readLine());
         }
 
         setTree(treeSize - 1);
+
         for (int i = 0; i < m + k; i++) {
             st = new StringTokenizer(br.readLine());
             long a = Integer.parseInt(st.nextToken());
-            int s = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             long e = Long.parseLong(st.nextToken());
             if (a == 1) {
-                changeVal(leftNodeStartIndex + s, e);
-            } else if (a == 2){
-                s = s + leftNodeStartIndex;
-                e = e + leftNodeStartIndex;
-                System.out.println(getSum(s, (int) e));
-            }
-        }
-    }
+                int index = leftStartIdx + b;
+                long diff = e - tree[index];
+                while (index > 0) {
+                    tree[index] += diff;
+                    index /= 2;
+                }
+            } else {
+                int start = leftStartIdx + b;
+                int end = leftStartIdx + (int) e;
+                long sum = 0;
+                while (start <= end) {
+                    if (start % 2 == 1) {
+                        sum += tree[start];
+                        start++;
+                    }
 
-    private static void changeVal(int index, long val) {
-        long diff = val - tree[index];
-        while (index > 0) {
-            tree[index] = tree[index] + diff;
-            index = index / 2;
-        }
-    }
+                    if (end % 2 == 0) {
+                        sum += tree[end];
+                        end--;
+                    }
+                    start /= 2;
+                    end /= 2;
+                }
+                System.out.println(sum);
+            }
 
-    private static long getSum(int s, int e) {
-        long partSum = 0;
-        while (s <= e) {
-            if (s % 2 == 1) {
-                partSum = partSum + tree[s];
-                s++;
-            }
-            if (e % 2 == 0) {
-                partSum = partSum + tree[e];
-                e--;
-            }
-            s = s / 2;
-            e = e / 2;
         }
-        return partSum;
     }
 
     private static void setTree(int i) {
