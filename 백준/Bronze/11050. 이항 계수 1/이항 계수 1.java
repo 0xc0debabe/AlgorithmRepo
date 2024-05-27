f7d53a9a9d9086fe847457f48e61341620bedb08
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-05-27 18:32:01 +0900

    [Bronze I] Title: 이항 계수 1, Time: 120 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -3,8 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
@@ -12,12 +13,9 @@ public class Main {
 
         int idx = 1;
         int answer = 1;
-        int tmp = 1;
         for (int i = n; i > n - k; i--) {
-            answer *= i % 10_007;
-            tmp *= idx++ % 10_007;
+            answer = answer * i / idx++;
         }
-
-        System.out.println(answer / tmp % 10_007);
+        System.out.println(answer);
     }
 }
