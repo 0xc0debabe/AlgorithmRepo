32da1438343209c3ff40a25ca280983cfeb7748f
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-05-29 02:41:02 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -3,25 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static long[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        dp = new long[n + 3];
-        dp[1] = 1;
-        dp[2] = 1;
-
-        fib(n);
-        System.out.println(dp[n]);
-    }
-
-    static long fib(int n) {
-        if (n == 0) return 0;
-        if (dp[n] > 0) {
-            return dp[n];
+        long[][] dp = new long[n + 1][2];
+        dp[1][1] = 1;
+        dp[1][0] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i - 1][1] + dp[i - 1][0];
+            dp[i][1] = dp[i - 1][0];
         }
-
-        return dp[n] = fib(n - 1) + fib(n - 2);
+        System.out.println(dp[n][0] + dp[n][1]);
     }
 }
