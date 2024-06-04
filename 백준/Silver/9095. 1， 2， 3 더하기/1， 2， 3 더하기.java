096346576609d0e5c624333234c4005a98065c9a
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-06-04 18:06:57 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= 10; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            System.out.println(dp[Integer.parseInt(br.readLine())]);
+        }
+    }
+}
