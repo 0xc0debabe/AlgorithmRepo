1019295410dda948d6141e44f2dacb23e488fef5
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-06-08 09:52:59 +0900

    [Silver III] Title: 터렛, Time: 136 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.java
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -11,6 +11,7 @@ public class Main {
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             int answer = 0;
+
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
@@ -19,22 +20,17 @@ public class Main {
             int y2 = Integer.parseInt(st.nextToken());
             int r2 = Integer.parseInt(st.nextToken());
 
-            System.out.println(findIntersectionPoints(x1, y1,  r1, x2, y2, r2));
+            double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
-        }
-    }
+            if (x1 == x2 && y1 == y2 && r1 == r2) {
+                answer = -1;
+            } else if (d == Math.abs(r1 - r2) || d == r1 + r2) {
+                answer = 1;
+            } else if (Math.abs(r1 - r2) < d && d < r1 + r2) {
+                answer = 2;
+            }
 
-    public static int findIntersectionPoints(int x1, int y1, int r1, int x2, int y2, int r2) {
-        double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-
-        if (d == 0 && r1 == r2) {
-            return -1; // Infinite number of points
-        } else if (d == r1 + r2 || d == Math.abs(r1 - r2)) {
-            return 1; // One intersection point
-        } else if (Math.abs(r1 - r2) < d && d < r1 + r2) {
-            return 2; // Two intersection points
-        } else {
-            return 0; // No intersection points
+            System.out.println(answer);
         }
     }
 }
