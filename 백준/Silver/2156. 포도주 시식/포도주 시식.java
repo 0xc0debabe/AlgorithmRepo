2a73b710268c4db76fc71a7868291f11c5f5a81d
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-06-12 23:25:06 +0900

    [Silver I] Title: 포도주 시식, Time: 152 ms, Memory: 14944 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int[] dp = new int[10001];
+        int answer = 0;
+        dp[1] = arr[1];
+        answer = Math.max(answer, dp[1]);
+
+
+        if (n >= 2) {
+            dp[2] = arr[2] + dp[1];
+            answer = Math.max(answer, dp[2]);
+        }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 3] + arr[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
+            answer = Math.max(answer, dp[i]);
+        }
+        System.out.println(answer);
+    }
+}
