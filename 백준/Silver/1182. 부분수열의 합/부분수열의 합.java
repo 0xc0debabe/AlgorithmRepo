eceaf45e4c4c1aab80832323a28ffd3ee6a965d4
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-06-14 11:54:23 +0900

    [Silver II] Title: 부분수열의 합, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,42 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.DateFormat;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, s;
-    static int[] arr;
     static int answer = 0;
-    static boolean[] isVisited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
-
-        arr = new int[n];
-        isVisited = new boolean[n];
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        dfs(0, 0);
+        dfs(n, 0, s, 0, arr);
         System.out.println(s == 0 ? answer - 1 : answer);
     }
 
-    static void dfs(int depth, int sum) {
-        if (depth == n) {
-            if (sum == s) answer++;
+    static void dfs(int n, int depth, int target, int sum, int[] arr) {
+        if (n == depth) {
+            if (target == sum) {
+                answer++;
+            }
             return;
         }
 
-        dfs(depth + 1, sum + arr[depth]);
-        dfs(depth + 1, sum);
+        dfs(n, depth + 1, target, sum + arr[depth], arr);
+        dfs(n, depth + 1, target, sum, arr);
     }
 }
