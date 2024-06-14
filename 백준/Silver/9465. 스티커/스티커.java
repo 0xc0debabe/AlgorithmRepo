4d346c868df813444cd71a2502bc5c8a5650ab32
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-06-14 19:22:42 +0900

    [Silver I] Title: 스티커, Time: 744 ms, Memory: 110476 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i = 0; i < t; i++) {
+            int size = Integer.parseInt(br.readLine());
+            int[][] board = new int[2][size + 1];
+            int[][] dp = new int[2][size + 1];
+
+            for (int j = 0; j < 2; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 1; k <= size; k++) {
+                    board[j][k] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dp[0][1] = board[0][1];
+            dp[1][1] = board[1][1];
+            for (int j = 2; j <= size; j++) {
+                dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + board[0][j];
+                dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + board[1][j];
+            }
+
+            System.out.println(Math.max(dp[0][size], dp[1][size]));
+        }
+    }
+}
