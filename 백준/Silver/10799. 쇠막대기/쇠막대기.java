afdc0eb09f8f8528663ede76966473c5ebfc92d5
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-06-16 13:49:29 +0900

    [Silver II] Title: 쇠막대기, Time: 184 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,31 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String pipe = br.readLine();
-        Deque<Character> stack = new ArrayDeque<>();
-        int result = 0;
-        for(int i=0; i<pipe.length();i++) {
-            if (pipe.charAt(i) == '(') {
-                stack.addFirst('(');
-            } else if (pipe.charAt(i) == ')') {
-                stack.pollLast();
-            }
-            if (i > 0) {
-                if (pipe.charAt(i - 1) == '(' && pipe.charAt(i) ==')') {
-                    result += stack.size();
-                } else if (pipe.charAt(i-1)==')' && pipe.charAt(i) ==')'){
-                    result++;
+        String s = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (s.charAt(i - 1) == ')') {
+                    answer++;
+                    stack.pop();
+                    continue;
                 }
+                stack.pop();
+                answer += stack.size();
             }
         }
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
 
