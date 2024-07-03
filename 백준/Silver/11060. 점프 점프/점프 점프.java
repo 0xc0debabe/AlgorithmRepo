6a9b9d97d230932e9fe2f439d27e529bdccc04b1
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-07-03 13:30:55 +0900

    [Silver II] Title: 점프 점프, Time: 144 ms, Memory: 14500 KB -BaekjoonHub

---
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        long[] dp = new long[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; j < i; j++) {
+                if (arr[j] + j >= i) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        if (dp[n] == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[n]);
+        }
+    }
+}
