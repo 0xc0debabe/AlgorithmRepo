05c5aafd2ddcb477162b33f637cc97a6edfbfa14
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-07-06 20:44:52 +0900

    [Silver III] Title: 합 구하기, Time: 684 ms, Memory: 48624 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()) + arr[i - 1];
+        }
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(arr[end] - arr[start - 1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
