c8a66e33da0be4d0ca35e20a069e581dd554602f
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-07-07 20:31:52 +0900

    [Silver III] Title: 개미, Time: 128 ms, Memory: 16180 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int endTime = Integer.parseInt(br.readLine());
+
+        int tmp1 = x + endTime;
+        int tmp2 = y + endTime;
+
+        int answer1, answer2;
+        if ((tmp1 / w) % 2 == 0) {
+            answer1 = tmp1 % w;
+        } else {
+            answer1 = w - tmp1 % w;
+        }
+
+        if ((tmp2 / h) % 2 == 0) {
+            answer2 = tmp2 % h;
+        } else {
+            answer2 = h - tmp2 % h;
+        }
+
+        System.out.println(answer1 + " " + answer2);
+    }
+}
