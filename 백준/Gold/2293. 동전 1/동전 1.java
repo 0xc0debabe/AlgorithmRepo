d0ea937401f174527facfb0ccf3c483202ad48da
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-07-13 22:26:43 +0900

    [Gold V] Title: 동전 1, Time: 116 ms, Memory: 18168 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -10,20 +10,29 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] coinArr = new int[n + 1];
+        int[] arr = new int[n + 1];
+
         for (int i = 1; i <= n; i++) {
-            coinArr[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
+
         int[][] dp = new int[n + 1][k + 1];
-        dp[0][0] = 1;
+
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                if (j < coinArr[i])
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+
+                if (j < arr[i]) {
                     dp[i][j] = dp[i - 1][j];
-                else
-                    dp[i][j] = dp[i][j - coinArr[i]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i][j - arr[i]] + dp[i - 1][j];
+                }
+
             }
         }
+
         System.out.println(dp[n][k]);
     }
 }
