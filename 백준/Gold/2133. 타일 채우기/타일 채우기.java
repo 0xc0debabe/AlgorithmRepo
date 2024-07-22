892a16883ead836754271eda3dcc4207ac5c9cb9
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-07-22 21:58:12 +0900

    [Gold IV] Title: 타일 채우기, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n + 5];
+        dp[0] = 1;
+        dp[2] = 3;
+
+        for (int i = 4; i <= n; i += 2) {
+            dp[i] = dp[i - 2] * dp[2];
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] = dp[i] + (dp[j] * 2);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
