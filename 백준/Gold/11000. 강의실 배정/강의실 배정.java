3641761b27b4f0a1ede8a8a8a7dad960a995a0b8
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-08-09 17:52:52 +0900

    [Gold V] Title: 강의실 배정, Time: 644 ms, Memory: 68348 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Node[] nodes = new Node[n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            nodes[i] = new Node(start, end);
+        }
+
+        Arrays.sort(nodes);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(nodes[0].end);
+        for (int i = 1; i < n; i++) {
+            if (pq.peek() <= nodes[i].start) {
+                pq.poll();
+            }
+
+            pq.offer(nodes[i].end);
+        }
+
+        System.out.println(pq.size());
+    }
+}
+
+class Node implements Comparable<Node>{
+    int start;
+    int end;
+
+    public Node(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int compareTo(Node node) {
+        if (this.start == node.start) {
+            return this.end - node.end;
+        }
+        return this.start - node.start;
+    }
+
+}
