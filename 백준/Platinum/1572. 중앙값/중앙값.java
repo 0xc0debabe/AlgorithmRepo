5d43a64dc7d4081a71c7375626f2070d0b5d1900
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-10-03 01:12:41 +0900

    [Platinum V] Title: 중앙값, Time: 372 ms, Memory: 32944 KB -BaekjoonHub

--- 백준/Platinum/1572. 중앙값/중앙값.java
+++ 백준/Platinum/1572. 중앙값/중앙값.java
@@ -2,73 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        // 중앙값을 빠르게 찾기 위한 두 개의 우선순위 큐
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // 중앙값 이하의 값들
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // 중앙값 이상의 값들
-
-        // K개의 값을 먼저 삽입하여 첫 번째 중앙값 찾기
-        for (int i = 0; i < K; i++) {
-            addNumber(arr[i], maxHeap, minHeap);
-        }
-
-        long sum = getMedian(maxHeap, minHeap); // 첫 번째 중앙값
-
-        for (int i = K; i < N; i++) {
-            // 새로운 값을 추가하고
-            addNumber(arr[i], maxHeap, minHeap);
-            // 윈도우에서 제외할 값을 제거한다
-            removeNumber(arr[i - K], maxHeap, minHeap);
-            // 중앙값을 구해서 더한다
-            sum += getMedian(maxHeap, minHeap);
-        }
-
-        System.out.println(sum);
-    }
-
-    // 새로운 숫자를 추가하는 함수
-    private static void addNumber(int num, PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap) {
-        if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
-            maxHeap.offer(num);
-        } else {
-            minHeap.offer(num);
-        }
-        balanceHeaps(maxHeap, minHeap);
-    }
-
-    // 윈도우에서 빠지는 숫자를 제거하는 함수
-    private static void removeNumber(int num, PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap) {
-        if (num <= maxHeap.peek()) {
-            maxHeap.remove(num);
-        } else {
-            minHeap.remove(num);
-        }
-        balanceHeaps(maxHeap, minHeap);
-    }
-
-    // 두 힙의 크기를 균형 맞추는 함수
-    private static void balanceHeaps(PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap) {
-        if (maxHeap.size() > minHeap.size() + 1) {
-            minHeap.offer(maxHeap.poll());
-        } else if (minHeap.size() > maxHeap.size()) {
-            maxHeap.offer(minHeap.poll());
-        }
-    }
-
-    // 현재 중앙값을 반환하는 함수
-    private static int getMedian(PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap) {
-        return maxHeap.peek();
-    }
+	
+	static int[] arr, tree;
+	static final int MAX = 65537;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		tree = new int[MAX*4];
+		arr = new int[n+1];
+		long sum = 0;
+		for(int i=1; i<=n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			update(0, MAX, 1, arr[i], 1);
+			if(i>=k) {
+				int x = query(0,MAX,1,(k+1)/2);
+				sum += x;
+				update(0, MAX, 1, arr[i-k+1], -1);
+			}
+		}
+		System.out.println(sum);
+	}
+	
+	static void update(int s, int e, int node, int idx, int val) {
+		if(idx < s|| e < idx) return;
+		tree[node] += val;
+		
+		if(s == e) return;
+		
+		int mid = (s+e)/2;
+		update(s, mid, node*2, idx, val);
+		update(mid+1, e, node*2+1, idx, val);
+	}
+	
+	static int query(int s, int e, int node ,int cnt) {
+		if(s == e) return s;
+		int mid = (s+e)/2;
+		if(tree[node*2] >= cnt) return query(s, mid, node*2, cnt);
+		else return query(mid+1, e, node*2+1, cnt-tree[node*2]);
+	}
 }
