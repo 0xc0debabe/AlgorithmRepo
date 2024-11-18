fcbc2ad5e3fe8b2b66e28858eb64dbe3b23a2603
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-11-18 20:55:47 +0900

    [level 2] Title: H-Index, Time: 0.93 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,16 +1,25 @@
+
 import java.util.Arrays;
 
 class Solution {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] citations = {5, 4, 7, 1, 2};
+        s.solution(citations);
+    }
+
     public int solution(int[] citations) {
         Arrays.sort(citations);
         int len = citations.length;
         int answer = 0;
         for(int i = 0; i < len; i++) {
-            if(citations[i] >= len - i){
-                answer = len - i;
-                break;
+            if(citations[i] <= len - i && citations[i] >= i + 1){
+                answer = citations[i];
+                answer = Math.max(answer, citations[i]);
             }
         }
+
+        System.out.println(answer);
         return answer;
     }
 }
