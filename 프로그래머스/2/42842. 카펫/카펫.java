061b45c90ec9bbc3effc7b38bcaa50579a6eb2f9
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-12-10 21:43:15 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,3 +1,6 @@
+
+import java.util.*;
+
 class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
@@ -6,18 +9,32 @@ class Solution {
         System.out.println(solution[1]);
     }
 
+
     public int[] solution(int brown, int yellow) {
+//        if (yellow % 2 == 1) {
+//            int a = yellow + 2;
+//            int b = 3;
+//            return new int[]{Math.max(a, b), Math.min(a, b)};
+//        }
+
+        int a = 0;
+        int b = 0;
+
         for (int i = 1; i <= yellow; i++) {
-            if (yellow % i == 0) {  // i가 yellow의 약수일 때
-                int width = i + 2;  // 가로 길이
-                int height = yellow / i + 2;  // 세로 길이
-                int brownNum = (width + height - 2) * 2;  // brownNum 계산
+            if (yellow % i == 0) {
+                int width = i + 2;
+                int height = yellow / i + 2;
+                int brownNum = width * height - yellow;
 
                 if (brownNum == brown) {
-                    return new int[]{Math.max(width, height), Math.min(width, height)};
+                    a = i + 2;
+                    b = yellow / i + 2;
+                    break;
                 }
             }
         }
-        return new int[]{0, 0};  // 기본적으로 잘못된 값이 들어온 경우
+
+        return new int[]{Math.max(a, b), Math.min(a, b)};
     }
+
 }
