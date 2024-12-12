ebce9ed15519c4bd8ac52367e70e5456b62c93c2
0xc0debabe <146945195+0xc0debabe@users.noreply.github.com>
2024-12-12 16:01:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.65 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -9,55 +9,50 @@ class Solution {
                 {1, 3},
                 {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}
         };
-
         sol.solution(a, ar);
     }
 
     List<List<Integer>> list = new ArrayList<>();
-    int answer = Integer.MAX_VALUE;
-
     public int solution(int n, int[][] wires) {
-        // 인접 리스트 초기화
         for (int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
         }
+
         for (int[] wire : wires) {
             list.get(wire[0]).add(wire[1]);
             list.get(wire[1]).add(wire[0]);
         }
 
-        // 모든 간선을 끊어보며 최소 차이 계산
-        for (int[] wire : wires) {
-            // 간선 끊기
-            list.get(wire[0]).remove((Integer) wire[1]);
-            list.get(wire[1]).remove((Integer) wire[0]);
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < wires.length; i++) {
+            int s = wires[i][0];
+            int e = wires[i][1];
 
-            // 한 쪽 서브트리 크기 계산
+            list.get(s).remove(Integer.valueOf(e));
+            list.get(e).remove(Integer.valueOf(s));
+
             boolean[] isVisited = new boolean[n + 1];
-            int subtreeSize = dfs(1, isVisited);
-
-            // 두 서브트리 크기 차이 계산
-            int diff = Math.abs(subtreeSize - (n - subtreeSize));
+            int part = dfs(s, isVisited);
+            int diff = Math.abs(n - part - part);
             answer = Math.min(answer, diff);
 
-            // 간선 복구
-            list.get(wire[0]).add(wire[1]);
-            list.get(wire[1]).add(wire[0]);
+            list.get(s).add(e);
+            list.get(e).add(s);
         }
 
         return answer;
     }
 
-    int dfs(int node, boolean[] isVisited) {
-        isVisited[node] = true;
-        int size = 1;
+    int dfs(int start, boolean[] isVisited) {
+        if (isVisited[start]) return 0;
 
-        for (int neighbor : list.get(node)) {
-            if (!isVisited[neighbor]) {
-                size += dfs(neighbor, isVisited);
-            }
+        int sum = 1;
+        isVisited[start] = true;
+        for (int next : list.get(start)) {
+            sum += dfs(next, isVisited);
         }
 
-        return size;
+        return sum;
     }
+
 }
