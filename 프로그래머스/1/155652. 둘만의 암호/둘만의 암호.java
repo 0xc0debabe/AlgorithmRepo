7c25664dd8469f16548999f979cc7751f9e42a41
Min Woo Hong <146945195+0xc0debabe@users.noreply.github.com>
2025-01-02 20:07:35 +0900

    [level 1] Title: 둘만의 암호, Time: 0.08 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,27 +1,38 @@
-public class Solution {
+
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] ar = {2, 3, 6};
+        String[] asd = {"ABACD", "BCEFD"};
+        String[] aa = {"ABCD", "AABB"};
+        System.out.println(sol.solution("aukks", "wbqd", 5));
+    }
+
     public String solution(String s, String skip, int index) {
-        // skip에 포함된 알파벳을 기록
-        boolean[] isSkipped = new boolean[26];
-        for (char ch : skip.toCharArray()) {
-            isSkipped[ch - 'a'] = true;
+        boolean[] skipAlphabet = new boolean[26];
+        for (char c : skip.toCharArray()) {
+            if (!skipAlphabet[c - 'a']) {
+                skipAlphabet[c - 'a'] = true;
+            }
         }
 
-        StringBuilder result = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        for (char current : s.toCharArray()) {
+            int step = index;
 
-        for (char ch : s.toCharArray()) {
-            int steps = index; // 이동해야 할 알파벳 수
-            char current = ch;
-
-            while (steps > 0) {
+            while (step > 0) {
                 current = (char) ((current - 'a' + 1) % 26 + 'a'); // 다음 알파벳으로 이동
-                if (!isSkipped[current - 'a']) { // skip에 포함되지 않으면 steps 감소
-                    steps--;
+                if (!skipAlphabet[current - 'a']) { // skip에 포함되지 않으면 steps 감소
+                    step--;
                 }
             }
 
-            result.append(current); // 결과에 변환된 문자 추가
+            sb.append(current);
         }
 
-        return result.toString();
+        return sb.toString();
     }
+
 }
