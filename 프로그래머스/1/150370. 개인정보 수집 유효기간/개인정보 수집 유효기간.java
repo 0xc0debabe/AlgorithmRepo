99712fa9619144125ab84579b6c53485011c82e7
Min Woo Hong <146945195+0xc0debabe@users.noreply.github.com>
2025-01-02 22:02:37 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.73 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,74 @@
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] ar = {2, 3, 6};
+        String[] asd = {"A 12"};
+        String[] aa = {"2010.01.01 A", "2019.12.17 A"};
+        Arrays.stream(sol.solution("2020.12.17", asd, aa)).forEach(System.out::println);
+    }
+
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        String[] split = today.split("\\.");
+        int yyyy = Integer.parseInt(split[0]);
+        int mm = Integer.parseInt(split[1]);
+        int dd = Integer.parseInt(split[2]);
+        LocalDate now = LocalDate.of(yyyy, mm, dd);
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String term : terms) {
+            String[] split1 = term.split(" ");
+            map.put(split1[0], Integer.parseInt(split1[1]));
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < privacies.length; i++) {
+            String privacy = privacies[i];
+            String[] split1 = privacy.split(" ");
+            int validPeriodMon = map.get(split1[1]);
+
+            String dateTime = split1[0];
+            String[] split2 = dateTime.split("\\.");
+            int joinY = Integer.parseInt(split2[0]);
+            int joinM = Integer.parseInt(split2[1]);
+            int joinD = Integer.parseInt(split2[2]);
+
+            if (joinD - 1 == 0) {
+                joinD = 28;
+                validPeriodMon--;
+            } else {
+                joinD -= 1;
+            }
+
+            joinM += validPeriodMon;
+            joinY += (joinM - 1) / 12;
+            joinM = (joinM - 1) % 12 + 1;
+            
+//            if (joinM + validPeriodMon > 12) {
+//                joinY += (joinM + validPeriodMon) / 12;
+//                joinM = (joinM + validPeriodMon) % 12;
+//            } else {
+//                joinM += validPeriodMon;
+//            }
+
+            LocalDate validPeriod = LocalDate.of(joinY, joinM, joinD);
+            if (now.isAfter(validPeriod)) {
+                list.add(i + 1);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+
+}
