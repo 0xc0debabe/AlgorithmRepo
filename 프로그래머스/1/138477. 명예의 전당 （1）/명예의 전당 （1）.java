90fc722af26a3c70c1f3ff29f82747db90f24b71
Min Woo Hong <146945195+0xc0debabe@users.noreply.github.com>
2025-01-03 18:10:34 +0900

    [level 1] Title: 명예의 전당 (1), Time: 6.40 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -10,22 +10,16 @@ class Solution {
         sol.solution(10, new int[]{1, 10, 2, 5, 6, 7, 8});
     }
 
-    public int[] solution(int k, int[] score) {
+    public int[] solution(int k, int[] scores) {
         int[] arr = new int[2001];
 
         List<Integer> list = new ArrayList<>();
-        Arrays.fill(arr, Integer.MAX_VALUE);
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < Math.min(k, score.length); i++) {
-            arr[i] = score[i];
-            min = Math.min(min, arr[i]);
-            list.add(min);
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for (int i = k - 1; i < score.length - 1; i++) {
-            arr[i + 1] = score[i + 1];
-            Arrays.sort(arr);
-            list.add(arr[i + 1 - k + 1]);
+        for (int score : scores) {
+            pq.add(score);
+            if (pq.size() > k) pq.poll();
+            list.add(pq.peek());
         }
 
         return list.stream().mapToInt(Integer::intValue).toArray();
