fee177cea214f9da4296d986f9858588635e1e7d
Min Woo Hong <146945195+0xc0debabe@users.noreply.github.com>
2025-01-09 21:57:48 +0900

    [level 1] Title: 모의고사, Time: 4.48 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,55 +1,47 @@
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] a = {1, 3, 2, 4, 2};
+        sol.solution(a);
+    }
+
+
+
     public int[] solution(int[] answers) {
-        int[] first = {1, 2, 3, 4, 5};
-        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int f = 0;
-        int s = 0;
-        int t = 0;
+        int[][] nums = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+
+        int[] correctNum = new int[3];
         int max = 0;
-
-        for (int i = 0; i < answers.length; i++) {
-            int answer = answers[i];
-            int fLen = first.length;
-            int sLen = second.length;
-            int tLen = third.length;
-
-            if (first[i % fLen] == answer) {
-                f++;
-            }
-
-            if (second[i % sLen] == answer) {
-                s++;
-            }
-
-            if (third[i % tLen] == answer) {
-                t++;
-            }
-
-            max = Math.max(f, Math.max(s, t));
+        for (int i = 0; i < 3; i++) {
+            correctNum[i] = isCorrect(i, answers, nums);
+            max = Math.max(max, correctNum[i]);
         }
 
         List<Integer> list = new ArrayList<>();
-        if (f == max) {
-            list.add(1);
-        }
-        if (s == max) {
-            list.add(2);
-        }
-        if (t == max) {
-            list.add(3);
-        }
-        Collections.sort(list);
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        for (int i = 0; i < correctNum.length; i++) {
+            if (correctNum[i] == max) list.add(i + 1);
         }
 
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
+
+    private int isCorrect(int idx, int[] answers, int[][] nums) {
+        int correctNum = 0;
+        for (int i = 0; i < answers.length; i++) {
+            int answer = answers[i];
+            int num = nums[idx][i % (nums[idx].length)];
+            if (answer == num) correctNum++;
+        }
+
+        return correctNum;
+    }
+
 }
