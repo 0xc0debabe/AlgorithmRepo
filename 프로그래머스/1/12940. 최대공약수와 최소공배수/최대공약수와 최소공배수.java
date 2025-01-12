8bf1982c79be723d011dbd2af0ac9d11a65b02b3
Min Woo Hong <kongminoo0421@gmail.com>
2025-01-12 17:00:55 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,19 @@
+
+class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int n, int m) {
+        int gcd = gcd(n, m);
+        int gcm = gcd * n / gcd * m / gcd;
+        return new int[]{gcd, gcm};
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+}
