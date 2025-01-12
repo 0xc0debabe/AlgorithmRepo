44958b579a57e416dd688c6d3b6fba8067c33763
Min Woo Hong <kongminoo0421@gmail.com>
2025-01-12 21:40:52 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.03 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,33 +1,27 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.solution(4444444444L));
+        System.out.println(sol.solution(4444444444L));  // 예시 테스트
     }
 
     public long solution(long n) {
-        Map<Long, Integer> map = new HashMap<>();
+        // Step 1: 각 숫자의 개수를 세기 위한 배열
+        int[] count = new int[10];
         while (n > 0) {
-            long l = n % 10;
-            map.put(l, map.getOrDefault(l, 0) + 1);
-            n /= 10;
+            count[(int) (n % 10)]++;  // 숫자 추출 후 개수 증가
+            n /= 10;  // 숫자 한 자리를 제거
         }
 
+        // Step 2: 내림차순으로 숫자 조합하여 결과 계산
         long answer = 0;
-        for (long i = 9; i >= 0; i--) {
-            if (map.containsKey(i)) {
-                int cnt = map.get(i);
-                while (cnt > 0) {
-                    answer += i;
-                    answer *= 10;
-                    cnt--;
-                }
+        for (int i = 9; i >= 0; i--) {
+            while (count[i] > 0) {
+                answer = answer * 10 + i;  // 해당 숫자 붙이기
+                count[i]--;  // 그 숫자의 개수 하나 감소
             }
         }
 
-        return answer / 10;
+        return answer;  // 결과 반환
     }
 }
