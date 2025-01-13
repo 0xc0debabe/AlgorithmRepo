f785b3554291a29d6552c332ebbfd832970019c9
Min Woo Hong <kongminoo0421@gmail.com>
2025-01-13 18:02:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.95 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] == arr[i]) continue;
+            list.add(arr[i]);
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+
+}
