d19f425dd26f9488cdd76255121410fb4fd2327e
Min Woo Hong <kongminoo0421@gmail.com>
2025-01-15 19:44:28 +0900

    [level 2] Title: 요격 시스템, Time: 335.27 ms, Memory: 181 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,49 +1,19 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[][] targets = {{1, 2},{2, 3}};
-        System.out.println(solution.solution(targets));
-    }
-
     public int solution(int[][] targets) {
-        PriorityQueue<Target> pq = new PriorityQueue<>();
-        for (int[] target : targets) pq.add(new Target(target[0], target[1]));
-
-        int startLine = Integer.MAX_VALUE;
-        int endLine = 0;
         int answer = 0;
-        while (!pq.isEmpty()) {
-            Target now = pq.poll();
-            if (now.s < endLine) {
-                startLine = now.s;
-                endLine = Math.min(endLine, now.e);
-                continue;
+        
+        Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
+        
+        int before = 0;
+        for(int i=0;i<targets.length;i++){
+            if(before <= targets[i][0]){
+                before = targets[i][1];
+                answer++;
             }
-
-            startLine = now.s;
-            endLine = now.e;
-            answer++;
         }
-
+        
         return answer;
     }
-
-    static class Target implements Comparable<Target>{
-        private int s;
-        private int e;
-
-        public Target(int s, int e) {
-            this.s = s;
-            this.e = e;
-        }
-
-        public int compareTo(Target target) {
-            if (this.s == target.s) return this.e - target.e;
-
-            return this.s - target.s;
-        }
-    }
-
 }
