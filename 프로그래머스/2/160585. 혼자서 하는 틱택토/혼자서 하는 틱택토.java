9c476564050654be96e50a194376796f4ce64617
Min Woo Hong <kongminoo0421@gmail.com>
2025-01-16 22:31:32 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.05 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,139 +1,64 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        String[] s1 = {
-                "O.X",
-                "O.X",
-                "O.."
-        };// 1
-
-        String[] s2 = {
-                "OOX",
-                "OOX",
-                "XXO"
-        };// 1
-
-        String[] s3 = {
-                "XOX",
-                "OXO",
-                "XOO"
-        };// 0
-
-        String[] s4 = {
-                "O.X",
-                "O..",
-                "O.."
-        };// 0
-
-        String[] s5 = {
-                "OOO",
-                "...",
-                "XXX"
-        };// 0
-
-        String[] s6 = {
-                "XO.",
-                "OXO",
-                "XOX"
-        };// 1
-
-        String[] s7 = {
-                "OOO",
-                "XOX",
-                "XXO"
-        };// 1
-
-        String[] s8 = {
-                "XOX",
-                "OXO",
-                "XOX"};// 0
-
-        String[] s9 = {
-                "OXO",
-                "XOX",
-                "OXO"};// 1
-//        System.out.println(sol.solution(s1));// 1
-//        System.out.println(sol.solution(s2));// 1
-//        System.out.println(sol.solution(s3));// 0
-//        System.out.println(sol.solution(s4));// 0
-//        System.out.println(sol.solution(s5));// 0
-//        System.out.println(sol.solution(s6));// 1
-        System.out.println(sol.solution(s7));// 1
-//        System.out.println(sol.solution(s8));// 0
-//        System.out.println(sol.solution(s9));// 1
-    }
-
     public int solution(String[] board) {
-        int Ocnt = 0;
-        int Xcnt = 0;
-        //O와 X의 개수를 센다.
-        for (int i = 0; i < 3; i++) {
-            Ocnt += countChar(board[i], 'O');
-            Xcnt += countChar(board[i], 'X');
-        }
+    int xCnt = 0;
+    int oCnt = 0;
 
-        //'X'가 'O'보다 많으면 규칙 위반
-        if (Xcnt > Ocnt) {
-            return 0;
-        }
-
-        //'O'가 'X'보다 2개이상 많으면 규칙 위반
-        if (Ocnt > Xcnt + 1) {
-            return 0;
-        }
-
-        //O가 완성 되었을 때 X가 O의 개수와 같으면 규칙 위반
-        if (hasWin(board, 'O')) {
-            if (Ocnt == Xcnt) {
-                return 0;
+    char[][] map = new char[3][3];
+    for (int i = 0; i < board.length; i++) {
+        for (int j = 0; j < board[i].length(); j++) {
+            map[i][j] = board[i].charAt(j);
+            if (map[i][j] == 'X') {
+                xCnt++;
+            } else if (map[i][j] == 'O') {
+                oCnt++;
             }
         }
-        //X가 완성 되었을 때 O가 X보다 1개 많으면 규칙 위반
-        if (hasWin(board, 'X')) {
-            if (Ocnt == Xcnt + 1) {
-                return 0;
-            }
-        }
-        return 1;
     }
 
-    private static int countChar(String str, char ch) {
-        return str.length() - str.replace(String.valueOf(ch), "").length();
+    // 턴 차이가 0 또는 1인지 확인
+    if (xCnt > oCnt || oCnt - xCnt > 1) {
+        return 0;
     }
 
-    private static boolean hasWin(String[] board, char ch) {
-        //가로 검사
-        for (int i = 0; i < 3; i++) {
-            if (board[i].charAt(0) == ch
-                    && board[i].charAt(1) == ch
-                    && board[i].charAt(2) == ch) {
-                return true;
-            }
+    // 승리 조건 확인
+    int xWinCnt = 0;
+    int oWinCnt = 0;
+
+    for (int i = 0; i < 3; i++) {
+        // 가로 검사
+        if (map[i][0] == map[i][1] && map[i][1] == map[i][2]) {
+            if (map[i][0] == 'X') xWinCnt++;
+            if (map[i][0] == 'O') oWinCnt++;
         }
-        //세로 검사
-        for (int i = 0; i < 3; i++) {
-            if (board[0].charAt(i) == ch
-                    && board[1].charAt(i) == ch
-                    && board[2].charAt(i) == ch) {
-                return true;
-            }
+
+        // 세로 검사
+        if (map[0][i] == map[1][i] && map[1][i] == map[2][i]) {
+            if (map[0][i] == 'X') xWinCnt++;
+            if (map[0][i] == 'O') oWinCnt++;
         }
-        //대각선 검사
-        if (board[0].charAt(0) == ch
-                && board[1].charAt(1) == ch
-                && board[2].charAt(2) == ch) {
-            return true;
-        }
-        if (board[0].charAt(2) == ch
-                && board[1].charAt(1) == ch
-                && board[2].charAt(0) == ch) {
-            return true;
-        }
-        return false;
     }
 
+    // 대각선 검사
+    if (map[0][0] == map[1][1] && map[1][1] == map[2][2]) {
+        if (map[0][0] == 'X') xWinCnt++;
+        if (map[0][0] == 'O') oWinCnt++;
+    }
+    if (map[0][2] == map[1][1] && map[1][1] == map[2][0]) {
+        if (map[0][2] == 'X') xWinCnt++;
+        if (map[0][2] == 'O') oWinCnt++;
+    }
 
+    // 승리 조건 위반 확인
+    if (xWinCnt > 0 && oWinCnt > 0) {
+        return 0; // 동시에 두 사람이 승리할 수 없음
+    }
+    if (oWinCnt > 0 && oCnt != xCnt + 1) {
+        return 0; // 'O'가 승리하면 턴 차이가 1이어야 함
+    }
+    if (xWinCnt > 0 && oCnt != xCnt) {
+        return 0; // 'X'가 승리하면 턴 개수가 같아야 함
+    }
+
+    return 1; // 규칙에 위배되지 않음
+}
 }
