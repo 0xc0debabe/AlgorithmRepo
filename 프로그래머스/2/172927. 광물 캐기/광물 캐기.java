10046f1a849c85d8a9cfff2e0b03db8c8b17ed80
Min Woo Hong <kongminoo0421@gmail.com>
2025-01-16 15:08:06 +0900

    [level 2] Title: 광물 캐기, Time: 18.93 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,38 +1,63 @@
-import java.util.Arrays;
-
 class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
         int[] arr = {1, 3, 2};
+//        String[] as = {"stone", "stone", "stone", "stone", "stone", "diamond", "diamond", "diamond", "diamond", "diamond"};
         String[] as = {"diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"};
-        sol.solution(arr, as);
+        System.out.println(sol.solution(arr, as));
     }
 
-    public int solution(int[] picks, String[] minerals) {
-        int[][] pirodo = {
-                {1, 1, 1},
-                {5, 1, 1},
-                {25, 5, 1}
-        };
+    private int answer = Integer.MAX_VALUE;
+    private final int[][] fatigue = {
+            {1, 1, 1}, // Diamond pickaxe
+            {5, 1, 1}, // Iron pickaxe
+            {25, 5, 1} // Stone pickaxe
+    };
 
-        picks = Arrays.stream(picks).map(i -> i * 5).toArray();
-        int idx = 0;
-        int answer = 0;
-        for (int i = 0; i < picks.length; i++) {
-            while (idx < minerals.length && picks[i] > 0) {
+    public int solution(int[] picks, String[] minerals) {
+        backtracking(picks, minerals, 0, 0);
+        return answer;
+    }
+
+    private void backtracking(int[] picks, String[] minerals, int idx, int sum) {
+        if (haveTool(picks) || idx > minerals.length) {
+            answer = Math.min(answer, sum);
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (picks[i] > 0) {
                 picks[i]--;
-                if (minerals[idx].equals("diamond")) {
-                    answer += pirodo[i][0];
-                } else if (minerals[idx].equals("iron")) {
-                    answer += pirodo[i][1];
-                } else {
-                    answer += pirodo[i][2];
-                }
-                idx++;
+                sum += cal(minerals, i, idx);
+                backtracking(picks, minerals, idx + 5, sum);
+                sum -= cal(minerals, i, idx);
+                picks[i]++;
+            }
+        }
+    }
+
+    private int cal(String[] minerals, int weapon, int idx) {
+        int sum = 0;
+        for (int i = idx; i < Math.min(idx + 5, minerals.length); i++) {
+            if (minerals[i].equals("diamond")) {
+                sum += fatigue[weapon][0];
+            } else if (minerals[i].equals("iron")) {
+                sum += fatigue[weapon][1];
+            } else {
+                sum += fatigue[weapon][2];
             }
         }
 
-        return answer;
+        return sum;
     }
+
+    private boolean haveTool(int[] picks) {
+        for (int pick : picks) {
+            if (pick > 0) return false;
+        }
+
+        return true;
+    }
+
 
 }
