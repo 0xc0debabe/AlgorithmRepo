0f72dc498455153e78a82a67b0a32c88cfadd06e
Min Woo Hong <kongminoo0421@gmail.com>
2025-01-22 01:28:05 +0900

    [level 2] Title: 시소 짝꿍, Time: 119.72 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] weights = {100, 180, 360, 100, 270};
+        System.out.println(sol.solution(weights));
+    }
+
+    public long solution(int[] weights) {
+        Map<Double, Integer> map = new HashMap<>();
+        Arrays.sort(weights);
+        long answer = 0;
+        for (int weight : weights) {
+            double a = weight*1.0;
+            double b = (weight*2.0)/3.0;
+            double c = (weight*1.0)/2.0;
+            double d = (weight*3.0)/4.0;
+            if(map.containsKey(a)) answer += map.get(a);
+            if(map.containsKey(b)) answer += map.get(b);
+            if(map.containsKey(c)) answer += map.get(c);
+            if(map.containsKey(d)) answer += map.get(d);
+            map.put((weight*1.0), map.getOrDefault((weight*1.0), 0)+1);
+//            map.put((double) (weight), map.getOrDefault(weight, 0) + 1);
+        }
+
+        return answer;
+    }
+
+}
