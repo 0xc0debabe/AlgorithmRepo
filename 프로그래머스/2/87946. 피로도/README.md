0a47e1ea843a8c5afa7ef7bd613c3ea8b67f215c
Min Woo Hong <kongminoo0421@gmail.com>
2025-02-04 17:25:56 +0900

    [level 2] Title: 피로도, Time: 2.91 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 84.3 MB, 시간: 3.64 ms
+메모리: 94.1 MB, 시간: 2.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 11일 19:23:59
+2025년 02월 04일 17:25:53
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,40 +1,26 @@
+    class Solution {
+        public static void main(String[] args) {
+            Solution sol = new Solution();
+        }
 
+        boolean[] isVisited;
+        int ret = 0;
+        public int solution(int k, int[][] dungeons) {
+            isVisited = new boolean[dungeons.length];
+            dfs(dungeons, k, 0);
+            return ret;
+        }
 
-import java.util.*;
+        private void dfs(int[][] dungeons, int health, int cnt) {
+            ret = Math.max(cnt, ret);
 
-class Solution {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int a = 80;
-        int[][] ar = {
-                {80, 20},
-                {50, 40},
-                {30, 10}
-        };
+            for (int i = 0; i < dungeons.length; i++) {
+                if (isVisited[i] || dungeons[i][0] > health) continue;
 
-        sol.solution(a, ar);
-    }
-
-    int answer = 0;
-    boolean[] isVisited;
-    public int solution(int k, int[][] dungeons) {
-        isVisited = new boolean[dungeons.length];
-        dfs(dungeons, k, 0);
-        return answer;
-    }
-
-    void dfs(int[][] dungeons, int nowFatigue, int depth) {
-        answer = Math.max(answer, depth);
-
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!isVisited[i]) {
-                if (nowFatigue < dungeons[i][0]) continue;
                 isVisited[i] = true;
-                dfs(dungeons, nowFatigue - dungeons[i][1], depth + 1);
+                dfs(dungeons, health - dungeons[i][1], cnt + 1);
                 isVisited[i] = false;
             }
         }
 
     }
-
-}
