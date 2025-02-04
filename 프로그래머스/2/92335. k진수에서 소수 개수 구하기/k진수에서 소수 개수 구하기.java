7e772640451f6761ccf2c30c272aff52e8d0c6d5
Min Woo Hong <kongminoo0421@gmail.com>
2025-02-04 16:58:49 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 14.84 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,29 +1,34 @@
+    import java.util.Arrays;
+
     class Solution {
         public static void main(String[] args) {
             Solution sol = new Solution();
-            System.out.println(sol.solution(437674, 3));
+            System.out.println(sol.solution(110011, 10));
         }
 
         public int solution(int n, int k) {
+            int range = 1_000_001;
+            boolean[] isPrime = new boolean[range];
+            Arrays.fill(isPrime, true);
+            isPrime[0] = false;
+            isPrime[1] = false;
+            for (int i = 0; i * i< range; i++) {
+                if (isPrime[i]) {
+                    for (int j = i * i; j < range; j += i) {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+
             String form = changeForm(n, k);
             String[] split = form.split("0");
             int result = 0;
-
             for (int i = 0; i < split.length; i++) {
                 if (split[i].isEmpty()) continue;
-
-                long target = Long.parseLong(split[i]);
-                if (target == 1) continue;
                 
-                boolean flag = true;
-                for (int j = 2; j <= Math.sqrt(target); j++) {
-                    if (target % j == 0) {
-                        flag = false;
-                        break;
-                    }
+                if (isPrime[Integer.parseInt(split[i])]) {
+                    result++;
                 }
-
-                if (flag) result++;
             }
 
             return result;
