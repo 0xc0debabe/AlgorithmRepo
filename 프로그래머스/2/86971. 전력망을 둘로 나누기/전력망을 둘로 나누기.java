776b58be78fd3ea8c5ed8eafd4ad88b9d61af81a
Min Woo Hong <kongminoo0421@gmail.com>
2025-02-05 14:03:42 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.42 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,53 +1,46 @@
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int a = 9;
-        int[][] ar = {
-                {1, 3},
-                {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}
-        };
-        sol.solution(a, ar);
+        int[][] a = {{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}};
+        sol.solution(9, a);
     }
 
     List<List<Integer>> list = new ArrayList<>();
     public int solution(int n, int[][] wires) {
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
-        }
-
+        for (int i = 0; i <= n; i++) list.add(new ArrayList<>());
         for (int[] wire : wires) {
             list.get(wire[0]).add(wire[1]);
             list.get(wire[1]).add(wire[0]);
         }
 
         int answer = Integer.MAX_VALUE;
-        for (int i = 0; i < wires.length; i++) {
-            int s = wires[i][0];
-            int e = wires[i][1];
+        for (int[] wire : wires) {
+            int s = wire[0];
+            int e = wire[1];
+            boolean[] isVisited = new boolean[n + 1];
 
             list.get(s).remove(Integer.valueOf(e));
             list.get(e).remove(Integer.valueOf(s));
 
-            boolean[] isVisited = new boolean[n + 1];
-            int part = dfs(s, isVisited);
-            int diff = Math.abs(n - part - part);
-            answer = Math.min(answer, diff);
+            int a = dfs(s, isVisited);
+            int b = n - a;
 
             list.get(s).add(e);
             list.get(e).add(s);
+
+            answer = Math.min(answer, Math.abs(a - b));
         }
 
         return answer;
     }
 
-    int dfs(int start, boolean[] isVisited) {
+    private int dfs(int start, boolean[] isVisited) {
         if (isVisited[start]) return 0;
-
-        int sum = 1;
         isVisited[start] = true;
+        int sum = 1;
         for (int next : list.get(start)) {
             sum += dfs(next, isVisited);
         }
@@ -56,3 +49,4 @@ class Solution {
     }
 
 }
+
