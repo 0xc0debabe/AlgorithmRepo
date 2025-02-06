17fc979e8495f975defdfc39da48d25924ad2c70
Min Woo Hong <kongminoo0421@gmail.com>
2025-02-07 00:45:09 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 61.34 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
+++ 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
@@ -1,51 +1,53 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    static int R, C;
-    static boolean[][][] isVisited;
-    static int[] dr = {-1, 0, 1, 0}; // 상, 우, 하, 좌
-    static int[] dc = {0, 1, 0, -1}; 
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] s = {"SL", "LR"};
+        solution.solution(s);
+    }
+
+    int n; int m;
+    boolean[][][] isVisited;
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, -1, 0, 1};
     public int[] solution(String[] grid) {
-        R = grid.length;
-        C = grid[0].length();
-        isVisited = new boolean[R][C][4]; // 3차원 방문 체크
+        n = grid.length;
+        m = grid[0].length();
+        isVisited = new boolean[4][n][m];
 
-        ArrayList<Integer> answer = new ArrayList<>();
-        
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 for (int d = 0; d < 4; d++) {
-                    if (!isVisited[i][j][d]) {
-                        answer.add(findCycle(grid, i, j, d));
+                    if (!isVisited[d][i][j]) {
+                        list.add(dfs(grid, d, i, j));
                     }
                 }
             }
         }
-        
-        Collections.sort(answer); // 오름차순 정렬
-        return answer.stream().mapToInt(i -> i).toArray();
+
+        return list.stream().sorted().mapToInt(i -> i).toArray();
     }
 
-    private static int findCycle(String[] grid, int r, int c, int d) {
-        int cnt = 0; // 사이클 길이 카운트
-
-        while (!isVisited[r][c][d]) {
-            isVisited[r][c][d] = true; // 방문 처리
+    private int dfs(String[] grid, int dir, int row, int col) {
+        int cnt = 0;
+        while (!isVisited[dir][row][col]) {
             cnt++;
-
-            // 현재 위치의 문자에 따라 방향 변경
-            if (grid[r].charAt(c) == 'L') {
-                d = (d + 3) % 4; // 좌회전
-            } else if (grid[r].charAt(c) == 'R') {
-                d = (d + 1) % 4; // 우회전
+            isVisited[dir][row][col] = true;
+            if (grid[row].charAt(col) == 'L') {
+                dir = (dir + 1) % 4;
+            } else if (grid[row].charAt(col) == 'R') {
+                dir = (dir + 3) % 4;
             }
 
-            // 다음 위치 계산 (격자를 벗어나면 반대편으로 이동)
-            r = (r + dr[d] + R) % R;
-            c = (c + dc[d] + C) % C;
+            row = (row + dx[dir] + n) % n;
+            col = (col + dy[dir] + m) % m;
         }
 
         return cnt;
     }
+
 }
