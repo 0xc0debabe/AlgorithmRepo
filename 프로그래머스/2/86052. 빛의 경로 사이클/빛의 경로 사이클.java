42565d03fd3ce5a7b4839b83dd4fd24a9bd6334f
Min Woo Hong <kongminoo0421@gmail.com>
2025-02-06 15:00:47 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 65.60 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
+++ 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
@@ -1,48 +1,51 @@
-import java.util.ArrayList;
- 
+import java.util.*;
+
 class Solution {
     static int R, C;
-    static int[] dr = { -1, 0, 1, 0 }, dc = { 0, -1, 0, 1 }; // 아래, 왼, 위, 오른
     static boolean[][][] isVisited;
- 
+    static int[] dr = {-1, 0, 1, 0}; // 상, 우, 하, 좌
+    static int[] dc = {0, 1, 0, -1}; 
+
     public int[] solution(String[] grid) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
- 
         R = grid.length;
         C = grid[0].length();
- 
-        isVisited = new boolean[R][C][4];
+        isVisited = new boolean[R][C][4]; // 3차원 방문 체크
+
+        ArrayList<Integer> answer = new ArrayList<>();
+        
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
                 for (int d = 0; d < 4; d++) {
-                    if (!isVisited[i][j][d])
-                        answer.add(light(grid, i, j, d ));
+                    if (!isVisited[i][j][d]) {
+                        answer.add(findCycle(grid, i, j, d));
+                    }
                 }
             }
         }
- 
-        return answer.stream().sorted().mapToInt(i -> i).toArray();
+        
+        Collections.sort(answer); // 오름차순 정렬
+        return answer.stream().mapToInt(i -> i).toArray();
     }
- 
-    private static int light(String[] grid, int r, int c, int d) {
-        int cnt = 0; // 이동거리
- 
-        while (true) {
-            if (isVisited[r][c][d])
-                break;
- 
-            cnt++;	// 거리증가
-            isVisited[r][c][d] = true; // 방문처리
- 
-            if (grid[r].charAt(c) == 'L')
-                d = d == 0 ? 3 : d - 1; // 좌회전
-            else if (grid[r].charAt(c) == 'R')
-                d = d == 3 ? 0 : d + 1; // 우회전
- 
+
+    private static int findCycle(String[] grid, int r, int c, int d) {
+        int cnt = 0; // 사이클 길이 카운트
+
+        while (!isVisited[r][c][d]) {
+            isVisited[r][c][d] = true; // 방문 처리
+            cnt++;
+
+            // 현재 위치의 문자에 따라 방향 변경
+            if (grid[r].charAt(c) == 'L') {
+                d = (d + 3) % 4; // 좌회전
+            } else if (grid[r].charAt(c) == 'R') {
+                d = (d + 1) % 4; // 우회전
+            }
+
+            // 다음 위치 계산 (격자를 벗어나면 반대편으로 이동)
             r = (r + dr[d] + R) % R;
             c = (c + dc[d] + C) % C;
         }
- 
+
         return cnt;
     }
 }
