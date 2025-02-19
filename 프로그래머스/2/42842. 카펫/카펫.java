a8a962f8137d881249b65e1da997e5a76232e7e2
Min Woo Hong <kongminoo0421@gmail.com>
2025-02-19 13:04:21 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,40 +1,25 @@
-
-import java.util.*;
-
 class Solution {
+
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] solution = sol.solution(18, 3);
-        System.out.println(solution[0]);
-        System.out.println(solution[1]);
+        Solution solution = new Solution();
+        solution.solution(8, 1);
     }
 
-
     public int[] solution(int brown, int yellow) {
-//        if (yellow % 2 == 1) {
-//            int a = yellow + 2;
-//            int b = 3;
-//            return new int[]{Math.max(a, b), Math.min(a, b)};
-//        }
-
-        int a = 0;
-        int b = 0;
-
-        for (int i = 1; i <= yellow; i++) {
+        int x = 0;
+        int y = 0;
+        for (int i = 1; i <= Math.max(yellow / 2, 1); i++) {
             if (yellow % i == 0) {
-                int width = i + 2;
-                int height = yellow / i + 2;
-                int brownNum = width * height - yellow;
-
-                if (brownNum == brown) {
-                    a = i + 2;
-                    b = yellow / i + 2;
+                if (brown == i * 2 + yellow / i * 2 + 4) {
+                    x = i + 2;
+                    y = (brown + yellow) / (i + 2);
                     break;
                 }
             }
         }
 
-        return new int[]{Math.max(a, b), Math.min(a, b)};
+        return new int[]{Math.max(x, y), Math.min(x, y)};
     }
 
 }
+
