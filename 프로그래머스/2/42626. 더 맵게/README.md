715e9ba5b8d685ca9ba2fa34361b08ea6635a56b
Min Woo Hong <kongminoo0421@gmail.com>
2025-02-21 12:32:34 +0900

    [level 2] Title: 더 맵게, Time: 1474.70 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 123 MB, 시간: 1595.55 ms
+메모리: 124 MB, 시간: 1474.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 21:16:04
+2025년 02월 21일 12:32:30
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,54 +1,33 @@
-
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 class Solution {
-    public static void main(String[] args){
-        Solution s = new Solution();
-        int[] sc = {3, 4, 5, 6, 7};
-        System.out.println(s.solution(sc, 10));
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {2,3};
+        System.out.println(solution.solution(arr, 7));
     }
 
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i : scoville) {
-            pq.add(i);
-        }
-        int cnt = 0;
+        for (int i : scoville) pq.add(i);
+        int answer = 0;
 
-        if (pq.peek() >= K) {
-            return 0;
-        }
-
-        while (pq.size() > 1) {
-            cnt++;
+        while (pq.size() >= 2) {
+            if (pq.peek() >= K) return answer;
 
             int first = pq.poll();
             int second = pq.poll();
-            int mixedFood = first + second * 2;
 
-            if (mixedFood >= K) {
-
-                if (!pq.isEmpty()) {
-                    int next = pq.peek();
-                    if (next >= K) {
-                        return cnt;
-                    } else {
-                        pq.add(mixedFood);
-                    }
-                } else {
-                    return cnt;
-                }
-
-            } else {
-                pq.add(mixedFood);
-            }
-
+            pq.add(first + second * 2);
+            answer++;
         }
 
+        if (!pq.isEmpty()) {
+            if (pq.poll() >= K) return answer;
+        }
         return -1;
     }
 
 }
+
