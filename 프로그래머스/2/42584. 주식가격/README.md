5caac53817c0c700982c958937c2dc45c84744fa
Min Woo Hong <kongminoo0421@gmail.com>
2025-02-22 13:14:06 +0900

    [level 2] Title: 주식가격, Time: 44.35 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 49.91 ms
+메모리: 73.7 MB, 시간: 44.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 02일 09:20:25
+2025년 02월 22일 13:14:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,51 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 
-import java.util.*;
-import java.util.stream.Collectors;
+class Solution {
 
-public class Solution {
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 2, 3};
-        Solution s = new Solution();
-        s.solution(a);
+        Solution solution = new Solution();
+        int[] arr = {95, 90, 99, 99, 80, 99};
+        int[] sp = {1, 1, 1, 1, 1, 1};
     }
+
     public int[] solution(int[] prices) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> y[0] - x[0]);
 
         int[] answer = new int[prices.length];
-
         for (int i = 0; i < prices.length; i++) {
             int price = prices[i];
 
-            while (!pq.isEmpty() && pq.peek().price > price) {
-                Node poll = pq.poll();
-                answer[poll.idx] = i - poll.idx;
+            while (!pq.isEmpty() && pq.peek()[0] > price) {
+                int[] poll = pq.poll();
+                int idx = poll[1];
+                answer[idx] = i - idx;
             }
 
-            pq.add(new Node(i, price));
+            pq.add(new int[]{price, i});
         }
 
         while (!pq.isEmpty()) {
-            Node poll = pq.poll();
-            answer[poll.idx] = prices.length - 1 - poll.idx;
+            int[] poll = pq.poll();
+            int idx = poll[1];
+            answer[idx] = prices.length - 1 - idx;
         }
 
         return answer;
     }
 
-    static class Node implements Comparable<Node> {
-        int idx;
-        int price;
-
-        public Node(int idx, int price) {
-            this.idx = idx;
-            this.price = price;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return o.price - this.price;
-        }
-    }
-
-
 }
+
