e4457672f4d4bf18bdf90b3d2b9c5e5c8b974a88
Min Woo Hong <kongminoo0421@gmail.com>
2025-03-05 21:27:54 +0900

    [level 3] Title: 이중우선순위큐, Time: 84.97 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,85 +1,39 @@
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.PriorityQueue;
 
 class Solution {
-    public static void main(String[] args){
-        Solution s = new Solution();
-        String[] sa = {"I -45",
-                "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
-        s.solution(sa);
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] s = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+        solution.solution(s);
     }
 
     public int[] solution(String[] operations) {
         PriorityQueue<Integer> asc = new PriorityQueue<>();
-        PriorityQueue<Integer> des = new PriorityQueue<>((x, y) -> y - x);
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (String s : operations) {
-            String[] split = s.split(" ");
-
+        PriorityQueue<Integer> desc = new PriorityQueue<>((x, y) -> y - x);
+        for (String operation : operations) {
+            String[] split = operation.split(" ");
             if (split[0].equals("I")) {
-                int v = Integer.parseInt(split[1]);
-                asc.add(v);
-                des.add(v);
-                map.put(v, map.getOrDefault(v, 0) + 1);
+                asc.add(Integer.parseInt(split[1]));
+                desc.add(Integer.parseInt(split[1]));
             } else {
-                if (split[1].equals("-1")) {
-                    while (!asc.isEmpty()) {
-                        int poll = asc.poll();
-                        int count = map.get(poll);
-
-                        if (count > 0) {
-                            map.put(poll, count - 1);
-                            break;
-                        }
-
-                    }
-                } else {
-                    while (!des.isEmpty()) {
-                        int poll = des.poll();
-                        int count = map.get(poll);
-
-                        if (count > 0) {
-                            map.put(poll, count - 1);
-                            break;
-                        }
+                if (!asc.isEmpty()) {
+                    if (split[1].equals("1")) {
+                        Integer poll = desc.poll();
+                        asc.remove(poll);
+                    } else {
+                        Integer poll = asc.poll();
+                        desc.remove(poll);
                     }
                 }
             }
         }
 
-        if (asc.isEmpty() || des.isEmpty()) {
-            return new int[]{0, 0};
-        } else {
-            int[] answer = new int[2];
-
-            while (!des.isEmpty()) {
-                int poll = des.poll();
-                int count = map.get(poll);
-
-                if (count > 0) {
-                    map.put(poll, count - 1);
-                    answer[0] = poll;
-                    answer[1] = poll;
-                    break;
-                }
-            }
-            while (!asc.isEmpty()) {
-                int poll = asc.poll();
-                int count = map.get(poll);
-
-                if (count > 0) {
-                    map.put(poll, count - 1);
-                    answer[1] = poll;
-                    break;
-                }
-            }
-
-            return answer;
-        }
+        int[] answer = new int[2];
+        if (asc.isEmpty()) return answer;
+        answer[0] = desc.poll();
+        answer[1] = asc.poll();
+        return answer;
     }
 
 }
