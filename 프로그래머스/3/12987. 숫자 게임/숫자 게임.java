e6c6cd6a69e84d8d528a6c3eced20e9972cfe252
Min Woo Hong <kongminoo0421@gmail.com>
2025-03-06 18:37:00 +0900

    [level 3] Title: 숫자 게임, Time: 106.14 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
 
@@ -12,30 +10,16 @@ class Solution {
     }
 
     public int solution(int[] A, int[] B) {
-        List<Integer> list = new ArrayList<>();
-        for (int i : B) list.add(i);
-        Collections.sort(list);
+        Arrays.sort(A);
+        Arrays.sort(B);
+
         int answer = 0;
-        for (int i = 0; i < A.length; i++) {
-            int target = A[i];
-
-            int left = 0;
-            int right = list.size() - 1;
-            while (left <= right) {
-                int mid = (left + right) / 2;
-
-                if (target >= list.get(mid)) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-
-            if (left < list.size() && list.get(left) > target) {
+        int idx = B.length - 1;
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (A[i] < B[idx]) {
+                idx--;
                 answer++;
             }
-
-            if (left < list.size() )list.remove(left);
         }
 
         return answer;
