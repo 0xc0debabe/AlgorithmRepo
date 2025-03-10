e33bef44b68477276d09280a38643383bb4bb6e8
Min Woo Hong <kongminoo0421@gmail.com>
2025-03-10 21:17:00 +0900

    [level 3] Title: 입국심사, Time: 46.06 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,35 +1,37 @@
-import java.util.*;
-
 class Solution {
+
     public static void main(String[] args) {
-        int n = 10;
-        int[] a = {7};
-        Solution s = new Solution();
-        System.out.println(s.solution(n, a));
+        Solution solution = new Solution();
+        int[] times = {7, 10};
+        System.out.println(solution.solution(6, times));
     }
 
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
-        long left = 1; // 최소 시간은 1분부터 시작
-        long right = (long) times[times.length - 1] * n; // 최대 시간은 최장 심사 시간 * n명
-        long answer = right; // 초기값은 최댓값으로 설정
-
+        long left = 0;
+        long right = 1_000_000_000L * 100_000;
+        long answer = 0;
         while (left <= right) {
-            long mid = left + (right - left) / 2;
+            long mid = (left + right) / 2;
 
             long capacity = 0;
-            for (int time : times) {
-                capacity += mid / time; // 현재 시간(mid) 내에 처리할 수 있는 인원 계산
-            }
+            for (int time : times) capacity += mid / time;
 
-            if (capacity >= n) { // 심사 가능한 인원이 n명을 만족하는 경우
-                answer = mid; // 가능한 최소 시간 갱신
-                right = mid - 1; // 더 작은 시간 탐색
-            } else { // 심사 가능한 인원이 부족한 경우
-                left = mid + 1; // 더 큰 시간 탐색
+//            if (n >= capacity) {
+//                answer = mid;
+//                left = mid + 1;
+//            } else {
+//                right = mid - 1;
+//            }
+//
+            if (n <= capacity) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
 
         return answer;
     }
+
 }
