a520ed9c61f77a2f2b4ca2fea6d54bed032c30fa
Min Woo Hong <kongminoo0421@gmail.com>
2025-03-10 16:51:57 +0900

    [level 3] Title: 섬 연결하기, Time: 1.30 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] arr = {{0, 1, 1}, {2, 3, 1}, {3, 1, 3}, {4, 0, 5}, {4, 2, 4}};
+        System.out.println(solution.solution(5, arr));
+    }
+
+    List<List<int[]>> list = new ArrayList<>();
+    public int solution(int n, int[][] costs) {
+        for (int i = 0; i < n; i++) list.add(new ArrayList<>());
+
+        int startVertex = 0;
+        int min = Integer.MAX_VALUE;
+        for (int[] cost : costs) {
+            int from = cost[0];
+            int to = cost[1];
+            int weight = cost[2];
+
+            list.get(from).add(new int[]{to, weight});
+            list.get(to).add(new int[]{from, weight});
+
+            if (weight < min) {
+                min = weight;
+                startVertex = from;
+            }
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> x[1] - y[1]);
+        pq.add(new int[]{startVertex, 0});
+        boolean[] isVisited = new boolean[n + 1];
+        int answer = 0;
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+            if (isVisited[now[0]]) continue;
+            isVisited[now[0]] = true;
+            answer += now[1];
+            
+            for (int[] next : list.get(now[0])) {
+                if (!isVisited[next[0]]) {
+                    pq.add(new int[]{next[0], next[1]});
+                }
+            }
+        }
+
+        return answer;
+    }
+
+
+}
