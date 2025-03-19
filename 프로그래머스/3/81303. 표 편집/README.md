c9181cba283986c05c09b068ce8cbcd7aea3f6bc
Min Woo Hong <kongminoo0421@gmail.com>
2025-03-19 15:34:43 +0900

    [level 3] Title: 표 편집, Time: 245.27 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/README.md
+++ 프로그래머스/3/81303. 표 편집/README.md
@@ -1,10 +1,10 @@
 # [level 3] 표 편집 - 81303 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81303) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81303#qna) 
 
 ### 성능 요약
 
-메모리: 105 MB, 시간: 785.40 ms
+메모리: 118 MB, 시간: 245.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 17일 22:25:59
+2025년 03월 19일 15:34:38
 
 ### 문제 설명
 
--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,36 +1,65 @@
 import java.util.*;
- 
+
 class Solution {
-    
-    public String solution(int n, int k, String[] cmd) { 
-        Stack<Integer> remove = new Stack<>();
-        int table_size = n;
-        
-        for(int i = 0; i < cmd.length; i++) {
-            char c = cmd[i].charAt(0);
- 
-            if(c == 'U') {
-                k -= Integer.valueOf(cmd[i].substring(2));
-            } else if(c == 'D') {
-                k += Integer.valueOf(cmd[i].substring(2));
-            } else if(c == 'C') {
-                remove.push(k);
-                table_size -= 1;
-                if(k == table_size) k -= 1;
-            } else {
-                int r = remove.pop(); 
-                if(k >= r) k += 1;
-                table_size += 1;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] cmd = {"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"};
+        System.out.println(solution.solution(8, 2, cmd));
+    }
+
+    public String solution(int n, int k, String[] cmd) {
+        int[] prev = new int[n];
+        for (int i = 0; i < n; i++) prev[i] = i - 1;
+        int[] next = new int[n];
+        for (int i = 0; i < n; i++) next[i] = i + 1;
+        char[] answer = new char[n];
+        Arrays.fill(answer, 'O');
+        int nowIdx = k;
+        Stack<int[]> stack = new Stack<>();
+        for (String command : cmd) {
+            String[] split = command.split(" ");
+
+            if (split[0].equals("U")) {
+                int num = Integer.parseInt(split[1]);
+                for (int i = 0; i < num; i++) {
+                    nowIdx = prev[nowIdx];
+                }
+
+            } else if (split[0].equals("D")) {
+                int num = Integer.parseInt(split[1]);
+                for (int i = 0; i < num; i++) {
+                    nowIdx = next[nowIdx];
+                }
+
+            } else if (split[0].equals("C")) {
+                stack.add(new int[]{prev[nowIdx], next[nowIdx], nowIdx});
+                if (prev[nowIdx] != -1) next[prev[nowIdx]] = next[nowIdx];
+                if (next[nowIdx] != n) prev[next[nowIdx]] = prev[nowIdx];
+
+                answer[nowIdx] = 'X';
+
+                if (next[nowIdx] == n) {
+                    nowIdx = prev[nowIdx];
+                    continue;
+                }
+                nowIdx = next[nowIdx];
+
+            } else if (split[0].equals("Z")){
+                int[] commandInfo = stack.pop();
+                int prevIdx = commandInfo[0];
+                int nextIdx = commandInfo[1];
+                int nowPos = commandInfo[2];
+                answer[nowPos] = 'O';
+
+                if (nextIdx != n) prev[nextIdx] = nowPos;
+                if (prevIdx != -1) next[prevIdx] = nowPos;
             }
         }
-        
+
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < table_size; i++) {
-            sb.append('O');
-        }
-        while(!remove.isEmpty()) {
-            sb.insert(remove.pop().intValue(), 'X');
-        }
+        for (char c : answer) sb.append(c);
         return sb.toString();
     }
+
+
 }
