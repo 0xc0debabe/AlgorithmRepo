040bc09bb00ae9c4ddc6ac2c3496863a1b366073
Min Woo Hong <kongminoo0421@gmail.com>
2025-05-20 21:45:20 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1260 ms, Memory: 81540 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -0,0 +1,54 @@
+import javax.swing.text.html.ListView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int[] count;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] isVisited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        count = new int[n + 1];
+        isVisited = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        fillVertexCount(r);
+
+        for (int i = 0; i < q; i++) {
+            System.out.println(count[Integer.parseInt(br.readLine())]);
+        }
+    }
+
+    private static void fillVertexCount(int parent) {
+        count[parent]++;
+        isVisited[parent] = true;
+        for (int child : graph.get(parent)) {
+            if (isVisited[child]) continue;
+            
+            fillVertexCount(child);
+            count[parent] += count[child];
+        }
+    }
+
+}
