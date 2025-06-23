2c3473a39af2eb460e44b9747660b2ebd1a12dd8
Min Woo Hong <kongminoo0421@gmail.com>
2025-06-23 15:07:22 +0900

    [Gold IV] Title: 고냥이, Time: 236 ms, Memory: 29280 KB -BaekjoonHub

---
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] countAlphabet = new int[26];
+        int pointer = 0;
+        int continuousCount = 0;
+        int answer = 0;
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (countAlphabet[c - 'a'] == 0) {
+                continuousCount++;
+            }
+            countAlphabet[c - 'a']++;
+
+            if (continuousCount > n) {
+                while (true) {
+                    char removedTarget = s.charAt(pointer++);
+                    int remainNumber = --countAlphabet[removedTarget - 'a'];
+
+                    if (remainNumber == 0) {
+                        continuousCount--;
+                        break;
+                    }
+                }
+            }
+
+            answer = Math.max(answer, countAll(countAlphabet));
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int countAll(int[] arr) {
+        return Arrays.stream(arr).sum();
+    }
+}
