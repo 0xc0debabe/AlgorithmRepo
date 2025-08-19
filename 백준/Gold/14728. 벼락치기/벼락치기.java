49605afb168293809cf25d59a3b2c3c97b21c074
Min Woo Hong <kongminoo0421@gmail.com>
2025-08-19 19:25:03 +0900

    [Gold V] Title: 벼락치기, Time: 132 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int N, T;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+
+        int[][] exam = new int[N][2];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            exam[i][0] = k;
+            exam[i][1] = s;
+        }
+
+        Arrays.sort(exam, (x, y) -> x[0] - y[0]);
+
+        int[][] dp = new int[N + 1][T + 1];
+        for(int i = 1; i <= N; i++) {
+            int expectedStudyTime = exam[i - 1][0];
+
+            for(int nowStudyTime = 1; nowStudyTime <= T; nowStudyTime++) {
+                dp[i][nowStudyTime] = dp[i - 1][nowStudyTime];
+
+                if(nowStudyTime >= expectedStudyTime){
+                    dp[i][nowStudyTime] = Math.max(dp[i][nowStudyTime], dp[i - 1][nowStudyTime - expectedStudyTime] + exam[i - 1][1]);
+                }
+            }
+        }
+
+        System.out.println(dp[N][T]);
+    }
+}
