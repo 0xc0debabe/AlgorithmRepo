50ff08fb857eda09c5852919de6b14602417d6b6
Min Woo Hong <kongminoo0421@gmail.com>
2025-08-27 19:12:22 +0900

    [Gold V] Title: 벼락치기, Time: 128 ms, Memory: 18144 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.java
+++ 백준/Gold/14728. 벼락치기/벼락치기.java
@@ -3,37 +3,37 @@ import java.util.*;
 
 class Main {
     static int N, T;
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         T = Integer.parseInt(st.nextToken());
-
-        int[][] exam = new int[N][2];
+        
+        int[] expStudyTime = new int[N];
+        int[] scores = new int[N];
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
-            int s = Integer.parseInt(st.nextToken());
-            exam[i][0] = k;
-            exam[i][1] = s;
+            expStudyTime[i] = Integer.parseInt(st.nextToken());
+            scores[i] = Integer.parseInt(st.nextToken());
         }
-
-        //Arrays.sort(exam, (x, y) -> x[0] - y[0]);
-
-        int[][] dp = new int[N + 1][T + 1];
+        
+        int[][] dp = new int[N + 1][10_001];
+        int answer = 0;
         for(int i = 1; i <= N; i++) {
-            int expectedStudyTime = exam[i - 1][0];
-
-            for(int nowStudyTime = 1; nowStudyTime <= T; nowStudyTime++) {
-                dp[i][nowStudyTime] = dp[i - 1][nowStudyTime];
-
-                if(nowStudyTime >= expectedStudyTime){
-                    dp[i][nowStudyTime] = Math.max(dp[i][nowStudyTime], dp[i - 1][nowStudyTime - expectedStudyTime] + exam[i - 1][1]);
+            int expected = expStudyTime[i - 1];
+            int score = scores[i - 1];
+            
+            for(int studyTime = 1; studyTime <= 10_000; studyTime++) {
+                dp[i][studyTime] = dp[i - 1][studyTime];
+                
+                if(studyTime >= expected) {
+                    dp[i][studyTime] = Math.max(dp[i][studyTime], dp[i - 1][studyTime - expected] + score);
                 }
+                
             }
         }
-
+        
         System.out.println(dp[N][T]);
     }
 }
