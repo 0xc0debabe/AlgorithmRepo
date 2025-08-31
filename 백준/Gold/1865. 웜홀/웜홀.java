032d9bdfd44784d1863832b69ec171a972439ae7
Min Woo Hong <kongminoo0421@gmail.com>
2025-08-31 15:21:28 +0900

    [Gold III] Title: 웜홀, Time: 348 ms, Memory: 21668 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,79 +1,74 @@
 import java.io.*;
 import java.util.*;
 
-class Edge {
-    int from, to, weight;
-
-    Edge(int from, int to, int weight) {
-        this.from = from;
-        this.to = to;
-        this.weight = weight;
-    }
-}
-
-public class Main {
+class Main {
     static int N, M, W;
-    static List<Edge> edges;
+    static List<Edge> graph = new ArrayList<>();
+    static int[] dist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int TC = Integer.parseInt(br.readLine());
-
+        int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-
-        while (TC-- > 0) {
+        while(t-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             W = Integer.parseInt(st.nextToken());
-
-            edges = new ArrayList<>();
-
-            // 도로 입력 (양방향)
-            for (int i = 0; i < M; i++) {
+            for(int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
-                int S = Integer.parseInt(st.nextToken());
-                int E = Integer.parseInt(st.nextToken());
-                int T = Integer.parseInt(st.nextToken());
-
-                edges.add(new Edge(S, E, T));
-                edges.add(new Edge(E, S, T));
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                int weight = Integer.parseInt(st.nextToken());
+                graph.add(new Edge(from, to, weight));
+                graph.add(new Edge(to, from, weight));
             }
-
-            // 웜홀 입력 (단방향, 시간 감소)
-            for (int i = 0; i < W; i++) {
+            for(int i = 0; i < W; i++) {
                 st = new StringTokenizer(br.readLine());
-                int S = Integer.parseInt(st.nextToken());
-                int E = Integer.parseInt(st.nextToken());
-                int T = Integer.parseInt(st.nextToken());
-
-                edges.add(new Edge(S, E, -T));
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                int weight = Integer.parseInt(st.nextToken());
+                graph.add(new Edge(from, to, -weight));
             }
 
-            if (hasNegativeCycle()) {
-                sb.append("YES\n");
-            } else {
-                sb.append("NO\n");
-            }
+            sb.append(isNegativeCycle() ? "YES" : "NO").append('\n');
+            graph.clear();
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 
-    static boolean hasNegativeCycle() {
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, 0); // 모든 노드 0으로 초기화
+    static boolean isNegativeCycle() {
+        dist = new int[N + 1];
+        Arrays.fill(dist, 1_000_000_000);
+        dist[1] = 0;
 
-        // N번 반복
-        for (int i = 1; i <= N; i++) {
-            for (Edge e : edges) {
-                if (dist[e.to] > dist[e.from] + e.weight) {
-                    dist[e.to] = dist[e.from] + e.weight;
-                    if (i == N) return true; // N번째 반복에서 갱신되면 음수 사이클 존재
+        for(int i = 0; i < N; i++) {
+            boolean isUpdate = false;
+            
+            for(Edge next : graph) {
+                if(dist[next.to] > dist[next.from] + next.weight) {
+                    dist[next.to] = dist[next.from] + next.weight;
+                    isUpdate = true;
+                    if(i == N - 1) return true;
                 }
             }
+            if(!isUpdate) return false;
         }
 
         return false;
     }
+
+    static class Edge {
+        int from;
+        int to;
+        int weight;
+
+        public Edge(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
 }
