d26788e96a2297231aa2fd2ab5dcf59703de01d2
Min Woo Hong <kongminoo0421@gmail.com>
2025-09-05 22:49:38 +0900

    [Gold V] Title: 무한 수열, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.java
+++ 백준/Gold/1351. 무한 수열/무한 수열.java
@@ -1,27 +1,24 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
     static long N, P, Q;
-    static Map<Long, Long> memo = new HashMap<>();
-
+    static Map<Long, Long> map = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Long.parseLong(st.nextToken());
         P = Long.parseLong(st.nextToken());
         Q = Long.parseLong(st.nextToken());
-
-        System.out.println(solve(N));
+        System.out.println(dfs(N));
     }
 
-    static long solve(long n) {
-        if (n == 0) return 1;
-        if (memo.containsKey(n)) return memo.get(n);
+    static long dfs(long n) {
+        if(n == 0) return 1;
+        if (map.containsKey(n)) return map.get(n);
 
-        long res = solve(n / P) + solve(n / Q);
-        memo.put(n, res);
+        long res = dfs(n / P) + dfs(n / Q);
+        map.put(n, res);
         return res;
     }
 }
